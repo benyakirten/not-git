d0ee1e5bf194ac6b6bbd6b346887c34d530d7173
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-03-14 12:09:35 -0600

    Complete ls-tree

--- src/ls_tree.rs
+++ src/ls_tree.rs
@@ -98,17 +98,6 @@ pub fn list_tree(args: &[String]) -> Result<(), anyhow::Error> {
 }
 
 fn parse_tree_files(decoded_content: Vec<u8>) -> Result<Vec<TreeFile>, anyhow::Error> {
-    // https://stackoverflow.com/questions/14790681/what-is-the-internal-format-of-a-git-tree-object
-    // https://git-scm.com/book/en/v2/Git-Internals-Git-Objects
-    // decoded_content might look like:
-    // tree 221\0100644 cat_file.rs\07��fh�M�6:c\u{8}����Ę�.100644 hash_object.rs\0y�Jϭj�N�[�Gdi�3�=Z�100644 init.rs\0ڜ��-u[�G{U�?\u{18}m�R�\u{4}�100644 lib.rs\0\u{f}����LmiP��Q���\0\u{1a}�100644 main.rs\0��X\u{b} y�\u{9d}��3V��g�\u{1c}��100644 utils.rs\0���֎��f#f.�$Ty'@�\u{6}�
-    // If you look right before main.rs, the sha contains a nulll byte \0, which makes splitting by null bytes
-    // not always have the file name at the end of the split. This is a workaround that might not be fullproof
-    // On the final version - we should parse each line into a struct
-    // Each line is in the format {numbers-mode} {file_name}\n{sha}
-    // Note that there's no separator beftween the sha and the numbers for the mode corresponding to the next line
-    // Therefore we may want to split for the first two whitespaces after the header
-
     let mut tree_files = vec![];
 
     let (_, mut body) = split_at_next_empty_byte(decoded_content)?;
@@ -127,8 +116,8 @@ fn split_at_next_empty_byte(contents: Vec<u8>) -> Result<(String, Vec<u8>), anyh
         Some(index) => index,
         None => {
             return Err(anyhow::anyhow!(
-                "Invalid tree object: Unable to find header"
-            ))
+                "Invalid tree object: Unable to find null byte in {contents:?}"
+            ));
         }
     };
     let (parseable, body) = contents.split_at(split_index + 1);
@@ -137,18 +126,17 @@ fn split_at_next_empty_byte(contents: Vec<u8>) -> Result<(String, Vec<u8>), anyh
 }
 
 fn parse_until_next_file(body: Vec<u8>) -> Result<(TreeFile, Vec<u8>), anyhow::Error> {
-    println!("RECEIVING A BODY");
-    println!("BODY: {:?}", body);
     let (mode_file_name, rest) = split_at_next_empty_byte(body)?;
     let (mode, file_name) = mode_file_name
         .split_once(' ')
         .ok_or_else(|| anyhow::anyhow!("Invalid tree object: Unable to find mode and file name"))?;
+    let file_name = file_name.replace('\0', "");
 
     let mut digits_acc: Vec<char> = vec![];
     let mut final_index = 0;
     for (i, byte) in rest.iter().enumerate() {
         if digits_acc.len() == 6 {
-            final_index = i;
+            final_index = i - 6;
             break;
         }
 
@@ -159,7 +147,14 @@ fn parse_until_next_file(body: Vec<u8>) -> Result<(TreeFile, Vec<u8>), anyhow::E
         }
     }
 
-    let (sha_bytes, rest) = rest.split_at(final_index);
+    // If the final_index is 0, it indicates we've reached EOF
+    let (sha_bytes, rest) = if final_index != 0 {
+        rest.split_at(final_index)
+    } else {
+        let empty_array: &[u8] = &[];
+        (rest.as_slice(), empty_array)
+    };
+
     let sha = hex::encode(sha_bytes);
     let file_type: FileType = FileType::from_mode(mode)?;
 
