044d5c993b58b330b627295cb0bd042ec8c86680
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-03-17 18:55:00 -0600

    Complete branch command

--- src/branch.rs
+++ src/branch.rs
@@ -4,29 +4,63 @@ pub struct BranchConfig {
     all: bool,
 }
 
+pub struct BranchOptions {
+    branches: Vec<String>,
+    current_head: String,
+}
+
 pub fn branch_command(args: &[String]) -> Result<(), anyhow::Error> {
     let config = parse_branch_config(args);
-    branch(config)?;
+    let branches = branch(config)?;
+
+    for file_name in branches.branches {
+        if file_name == branches.current_head {
+            println!("\x1b[92m * {}\x1b[0m", file_name);
+        } else {
+            println!("   {}", file_name);
+        }
+    }
 
     Ok(())
 }
 
-pub fn branch(config: BranchConfig) -> Result<Vec<String>, anyhow::Error> {
+pub fn branch(config: BranchConfig) -> Result<BranchOptions, anyhow::Error> {
     // TODO: Handle tags
 
     let head_path: PathBuf = ["not-git", "refs", "heads"].iter().collect();
-    let mut branches = collect_branches(head_path)?;
+    let mut branches = collect_branches(vec![], head_path)?;
 
     if config.all {
         let remotes_path: PathBuf = ["not-git", "refs", "remotes"].iter().collect();
-        let remotes_branches = collect_branches(remotes_path)?;
+        let remotes_branches = collect_branches(vec![], remotes_path)?;
         branches.extend(remotes_branches);
     }
 
-    Ok(branches)
+    let head = ["not-git", "HEAD"].iter().collect::<PathBuf>();
+    let head = std::fs::read_to_string(head)?;
+
+    let head_ref = head
+        .split("refs/heads/")
+        .last()
+        .ok_or_else(|| {
+            anyhow::anyhow!(format!(
+                "Invalid HEAD file. Expected ref: refs/heads<branch_name>, got {}",
+                head
+            ))
+        })?
+        .trim();
+
+    let branch_options = BranchOptions {
+        branches,
+        current_head: head_ref.to_string(),
+    };
+    Ok(branch_options)
 }
 
-fn collect_branches(path: PathBuf) -> Result<Vec<String>, anyhow::Error> {
+fn collect_branches(
+    preceding_dirs: Vec<String>,
+    path: PathBuf,
+) -> Result<Vec<String>, anyhow::Error> {
     let mut files: Vec<String> = vec![];
     for p in path.read_dir()? {
         if p.is_err() {
@@ -43,13 +77,17 @@ fn collect_branches(path: PathBuf) -> Result<Vec<String>, anyhow::Error> {
         }
 
         if p.file_type().unwrap().is_dir() {
-            let dir_files = collect_branches(p.path());
+            let mut new_dir = preceding_dirs.to_vec();
+            new_dir.push(p.file_name().to_string_lossy().to_string());
+            let dir_files = collect_branches(new_dir, p.path());
             match dir_files {
                 Ok(mut f) => files.append(&mut f),
                 Err(e) => return Err(e),
             }
         } else {
-            files.push(p.file_name().to_string_lossy().to_string());
+            let file_name = p.file_name().to_string_lossy().to_string();
+            let branch_name = preceding_dirs.join("/") + "/" + &file_name;
+            files.push(branch_name);
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-use not_git::{cat_file, commit_tree, hash_object, init, ls_tree, update_refs, write_tree};
+use not_git::{branch, cat_file, commit_tree, hash_object, init, ls_tree, update_refs, write_tree};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -19,6 +19,7 @@ fn main() {
         "write-tree" => write_tree::write_tree(&args[2..]),
         "commit-tree" => commit_tree::create_commit_tree(&args[2..]),
         "update-refs" => update_refs::update_refs_command(&args[2..]),
+        "branch" => branch::branch_command(&args[2..]),
         _ => Err(anyhow::anyhow!(format!("Unknown command {}", command))),
     };
 
