c779dfcaede99da82f6601f508bd9fe2a52907b9
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-03-18 11:36:22 -0600

    Split git branch into list, create and delete commands in theory

--- src/branch.rs
+++ src/branch.rs
@@ -1,20 +1,90 @@
 use std::path::PathBuf;
 
-use crate::utils::get_head_ref;
+use crate::{
+    file_hash::FileHash,
+    update_refs::{self, UpdateRefsConfig},
+    utils::{get_head_ref, read_from_file},
+};
 
-pub struct BranchConfig {
-    all: bool,
+pub enum BranchConfig {
+    List(bool),
+    Delete(String),
+    Create(String),
 }
 
-pub struct BranchOptions {
+pub struct ListBranchOptions {
     branches: Vec<String>,
     head_ref: String,
 }
 
-pub fn branch_command(args: &[String]) -> Result<(), anyhow::Error> {
-    let config = parse_branch_config(args);
-    let branches = list_branches(config)?;
+pub struct DeleteBranchResults {
+    path: PathBuf,
+    sha: String,
+}
 
+pub fn branch_command(args: &[String]) -> Result<(), anyhow::Error> {
+    let config = parse_branch_config(args)?;
+    match config {
+        BranchConfig::List(list_all_branches) => {
+            let branches = list_branches(list_all_branches)?;
+            print_branches(branches)
+        }
+        BranchConfig::Delete(branch_name) => {
+            let results = delete_branch(branch_name)?;
+            println!("Deleted branch {:?} (was {})", results.path, results.sha);
+            Ok(())
+        }
+        BranchConfig::Create(branch_name) => create_branch(branch_name),
+    }
+}
+
+fn delete_branch(branch_name: String) -> Result<DeleteBranchResults, anyhow::Error> {
+    let head_ref = get_head_ref()?;
+    if branch_name == head_ref {
+        return Err(anyhow::anyhow!(
+            "Cannot delete the branch you are currently on"
+        ));
+    }
+
+    let path: PathBuf = ["not-git", "refs", "heads", branch_name.as_str()]
+        .iter()
+        .collect();
+    if !path.exists() {
+        return Err(anyhow::anyhow!("Branch {} does not exist", branch_name));
+    }
+
+    let contents = std::fs::read_to_string(&path)?;
+    let contents = FileHash::from_sha(contents)?;
+
+    std::fs::remove_file(&path)?;
+
+    Ok(DeleteBranchResults {
+        path,
+        sha: contents.full_hash(),
+    })
+}
+
+fn create_branch(branch_name: String) -> Result<(), anyhow::Error> {
+    let head_ref = get_head_ref()?;
+    let path: PathBuf = ["not-git", "refs", "heads", &head_ref].iter().collect();
+    if path.exists() {
+        return Err(anyhow::anyhow!("Branch {} already exists", branch_name));
+    }
+
+    let head_commit = read_from_file(&path)?;
+    let head_commit = String::from_utf8(head_commit)?;
+    let head_commit = FileHash::from_sha(head_commit)?;
+
+    let config = UpdateRefsConfig {
+        commit_hash: head_commit,
+        path: PathBuf::from(branch_name),
+    };
+    update_refs::update_refs(config)?;
+
+    Ok(())
+}
+
+fn print_branches(branches: ListBranchOptions) -> Result<(), anyhow::Error> {
     for file_name in branches.branches {
         if file_name == branches.head_ref {
             println!("\x1b[92m * {}\x1b[0m", file_name);
@@ -26,13 +96,13 @@ pub fn branch_command(args: &[String]) -> Result<(), anyhow::Error> {
     Ok(())
 }
 
-pub fn list_branches(config: BranchConfig) -> Result<BranchOptions, anyhow::Error> {
+pub fn list_branches(list_all_branches: bool) -> Result<ListBranchOptions, anyhow::Error> {
     // TODO: Handle tags
 
     let head_path: PathBuf = ["not-git", "refs", "heads"].iter().collect();
     let mut branches = collect_branches(vec![], head_path)?;
 
-    if config.all {
+    if list_all_branches {
         let remotes_path: PathBuf = ["not-git", "refs", "remotes"].iter().collect();
         let remotes_branches = collect_branches(vec![], remotes_path)?;
         branches.extend(remotes_branches);
@@ -40,7 +110,7 @@ pub fn list_branches(config: BranchConfig) -> Result<BranchOptions, anyhow::Erro
 
     let head_ref = get_head_ref()?;
 
-    let branch_options = BranchOptions { branches, head_ref };
+    let branch_options = ListBranchOptions { branches, head_ref };
     Ok(branch_options)
 }
 
@@ -81,7 +151,21 @@ fn collect_branches(
     Ok(files)
 }
 
-fn parse_branch_config(args: &[String]) -> BranchConfig {
-    let all = args.contains(&"-a".to_string());
-    BranchConfig { all }
+fn parse_branch_config(args: &[String]) -> Result<BranchConfig, anyhow::Error> {
+    if args.is_empty() || args[0] == "--list" {
+        return Ok(BranchConfig::List(false));
+    }
+
+    if args[0] == "-a" {
+        return Ok(BranchConfig::List(true));
+    }
+
+    if args[0] == "-d" {
+        if args.len() < 2 {
+            return Err(anyhow::anyhow!("Branch name not provided"));
+        }
+        return Ok(BranchConfig::Delete(args[1].to_string()));
+    }
+
+    Ok(BranchConfig::Create(args[0].to_string()))
 }
