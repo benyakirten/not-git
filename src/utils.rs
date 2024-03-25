694d89bdc8ad414bc852b14f2c088ed57c2ec1a0
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-03-25 10:48:33 -0600

    Begin to write utils tests [ci skip]

--- src/utils.rs
+++ src/utils.rs
@@ -42,7 +42,7 @@ pub fn get_head_ref() -> Result<String, anyhow::Error> {
         .last()
         .ok_or_else(|| {
             anyhow::anyhow!(format!(
-                "Invalid HEAD file. Expected ref: refs/heads<branch_name>, got {}",
+                "Invalid HEAD file. Expected ref: refs/heads/<branch_name>, got {}",
                 head
             ))
         })?
@@ -57,4 +57,61 @@ pub fn split_header_from_contents(content: &[u8]) -> Result<(&[u8], &[u8]), anyh
     let header = split_content.next().context("Getting header")?;
     let body = split_content.next().context("Getting body")?;
     Ok((header, body))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn split_header_from_contents_returns_header_and_body_success() {
+        let content = b"blob 5\0hello";
+        let (header, body) = split_header_from_contents(content).unwrap();
+        assert_eq!(header, b"blob 5");
+        assert_eq!(body, b"hello");
+    }
+
+    #[test]
+    fn split_header_from_body_error_no_null_byte() {
+        let content = b"blob 5hello";
+        let result = split_header_from_contents(content);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_create_header_success() {
+        let object_type = ObjectType::Blob;
+        let file = b"file content";
+        let header = create_header(&object_type, file);
+        assert_eq!(header, b"blob 12\0");
+    }
+
+    #[test]
+    fn test_split_header_from_contents_success() {
+        let content = b"blob 5\0hello";
+        let (header, body) = split_header_from_contents(content).unwrap();
+        assert_eq!(header, b"blob 5");
+        assert_eq!(body, b"hello");
+    }
+
+    #[test]
+    fn test_split_header_from_contents_error() {
+        let content = b"blob 5hello";
+        let result = split_header_from_contents(content);
+        assert!(result.is_err());
+    }
+
+    // #[test]
+    // fn test_get_head_ref() {
+    //     let head_ref = get_head_ref().unwrap();
+    //     assert_eq!(head_ref, "branch_name");
+    // }
+
+    // #[test]
+    // fn test_read_from_file_success() {
+    //     let content = read_from_file("/path/to/file.txt").unwrap();
+    //     assert_eq!(content, b"file content");
+    // }
+
+    // fn test_read_from_file_error_if_file_missing() {}
 }
