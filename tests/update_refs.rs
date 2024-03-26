5da9c32b4101f81f64cdb92ffcf7ed00a213375c
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-03-26 08:38:13 -0600

    Set up update_refs tests [ci skip]

---
+++ tests/update_refs.rs
@@ -0,0 +1,19 @@
+mod common;
+
+#[test]
+fn update_refs_success() {
+    let path = common::TestPath::new();
+    // TODO
+}
+
+#[test]
+fn update_refs_create_branch_if_not_exists() {
+    let path = common::TestPath::new();
+    // TODO
+}
+
+#[test]
+fn update_refs_failure_missing_head() {
+    let path = common::TestPath::new();
+    // TODO
+}
