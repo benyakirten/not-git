08bd200357262f839c4b5cf067271d843cbeca7c
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-03-26 15:30:33 -0600

    Maybe fix test 2

--- tests/object_type.rs
+++ tests/object_type.rs
@@ -8,17 +8,19 @@ mod common;
 fn from_entry() {
     let path = common::TestPath::new();
 
-    let sub_dir = path.join(&"B_sub_dir");
+    let sub_dir = path.join(&"sub_dir");
     fs::create_dir(&sub_dir).unwrap();
 
-    let sub_file = path.join(&"A_sub_file");
+    let mut dir = path.0.read_dir().unwrap();
+    let object_type = ObjectType::from_entry(&dir.next().unwrap().unwrap()).unwrap();
+    assert_eq!(object_type, ObjectType::Tree);
+
+    fs::remove_dir(&sub_dir).unwrap();
+
+    let sub_file = path.join(&"sub_file");
     fs::File::create(sub_file).unwrap();
 
     let mut dir = path.0.read_dir().unwrap();
-
     let object_type = ObjectType::from_entry(&dir.next().unwrap().unwrap()).unwrap();
     assert_eq!(object_type, ObjectType::Blob);
-
-    let object_type = ObjectType::from_entry(&dir.next().unwrap().unwrap()).unwrap();
-    assert_eq!(object_type, ObjectType::Tree);
 }
